211b4e8007507e711ac8e95b8273f9ab2b9c8ad6
Mukund Lakshman <yaymukund@gmail.com>
2015-06-09 18:07:25 -0700

    wSeparate controls from player

---
+++ src/controls.js
@@ -0,0 +1,33 @@
+import Player from './player';
+
+export default class Controls {
+  constructor() {
+    this.player = new Player();
+  }
+
+  play(url, when=0) {
+    this.player.play(url, when);
+  }
+
+  pause() {
+    this.player.pause();
+  }
+
+  resume() {
+    this.player.resume();
+  }
+
+  setVolume(volume) {
+    this.player.setVolume(volume);
+  }
+
+  mute() {
+    this.oldVolume = this.player.getVolume();
+    this.setVolume(0);
+  }
+
+  unmute() {
+    this.setVolume(this.oldVolume || 1);
+    this.oldVolume = null;
+  }
+}
--- src/player.js
+++ src/player.js
@@ -1,19 +1,67 @@
-import {
-  decode,
-  fetch,
-  play
-} from './webaudio';
+import { libs } from './settings';
+
+let _fetch = function(url) {
+  let request = new XMLHttpRequest(),
+      deferred = libs.RSVP.defer();
+
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+
+  request.onload = () => {
+    deferred.resolve(request.response);
+  };
+
+  request.onerror = () => {
+    let error = new Error(`Could not fetch url ${url}`);
+    deferred.reject(error);
+  };
+
+  request.send();
+  return deferred.promise;
+};
+
+let _decode = function(audioContext, arrayBuffer) {
+  let deferred = libs.RSVP.defer();
+
+  audioContext.decodeAudioData(arrayBuffer, (audioBuffer) => {
+    deferred.resolve(audioBuffer);
+  }, deferred.reject);
+
+  return deferred.promise;
+};
 
 export default class Player {
   constructor() {
-    this.context = new AudioContext();
+    this.audioContext = new AudioContext();
+    this.gainNode = this.audioContext.createGain();
+    this.gainNode.gain.value = 1;
+    this.gainNode.connect(this.audioContext.destination);
   }
 
   play(url, when=0) {
-    fetch(url).then(arrayBuffer => {
-      return decode(this.context, arrayBuffer);
+    return _fetch(url).then(arrayBuffer => {
+      return _decode(this.audioContext, arrayBuffer);
     }).then(audioBuffer => {
-      play(this.context, audioBuffer, when);
+      let source = this.audioContext.createBufferSource();
+      source.buffer = audioBuffer;
+      source.connect(this.gainNode);
+      source.start(when);
     });
+  }
+
+  pause() {
+    return this.audioContext.suspend();
+  }
+
+  resume() {
+    return this.audioContext.resume();
+  }
+
+  setVolume(volume) {
+    this.gainNode.gain.value = volume;
+  }
+
+  getVolume() {
+    return this.gainNode.gain.value;
   }
 }
